8f2642a82fb01f75a08a8e5bbbd9f0162b17b945
mBisaga13 <mbisaga@student.agh.edu.pl>
2026-06-13 11:42:30 +0200

    Update Ocena.cpp

--- src/Ocena.cpp
+++ src/Ocena.cpp
@@ -1,7 +1,13 @@
 #include "Ocena.h"
 Ocena::Ocena(int ocena, std::string komentarz, Czytelnik* czytelnik, Ksiazka* ksiazka) : ocena(ocena), komentarz(komentarz), czytelnik(czytelnik), ksiazka(ksiazka) {}
-void Ocena::edytujOcene(int ocena, std::string komentarz) {}
-void Ocena::usunOcene() {}
+void Ocena::edytujOcene(int nowaOcena, std::string nowyKomentarz) {
+    ocena = nowaOcena;
+    komentarz = nowyKomentarz;
+}
+void Ocena::usunOcene() {
+    ocena = 0;
+    komentarz.clear();
+}
 int Ocena::getOcena() { return ocena; }
 std::string Ocena::getKomentarz() { return komentarz; }
 Czytelnik* Ocena::getCzytelnik() { return czytelnik; }
