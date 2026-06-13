f5e8950f1f664883acfe9b8482d4e592723fc1c4
mBisaga13 <mbisaga@student.agh.edu.pl>
2026-06-13 11:39:36 +0200

    Update Ksiazka.cpp

--- src/Ksiazka.cpp
+++ src/Ksiazka.cpp
@@ -1,10 +1,23 @@
 #include "Ksiazka.h"
+#include "Egzemplarz.h"
+#include "Ocena.h"
 Ksiazka::Ksiazka(std::string tytul, Autor* autor, std::string gatunek) : tytul(tytul), autor(autor), gatunek(gatunek) {}
-std::vector<Ocena*> Ksiazka::sprawdzOceny() { return {}; }
-void Ksiazka::dodajEgzemplarz(int numer, std::string lokalizacja) {}
+std::vector<Ocena*> Ksiazka::sprawdzOceny() { return oceny; }
+void Ksiazka::dodajEgzemplarz(int numer, std::string lokalizacja) {
+    egzemplarze.push_back(new Egzemplarz(this, lokalizacja, numer));
+}
 void Ksiazka::usunKsiazke() {}
-void Ksiazka::recenzujKsiazke(int ocena, std::string komentarz) {}
-Egzemplarz* Ksiazka::wolnyEgzemplarz() { return nullptr; }
+void Ksiazka::recenzujKsiazke(int ocena, std::string komentarz) {
+    oceny.push_back(new Ocena(ocena, komentarz, nullptr, this));
+}
+Egzemplarz* Ksiazka::wolnyEgzemplarz() {
+    for (Egzemplarz* egzemplarz : egzemplarze) {
+        if (egzemplarz != nullptr && egzemplarz->getStatus() == 1) {
+            return egzemplarz;
+        }
+    }
+    return nullptr;
+}
 std::string Ksiazka::getTytul() { return tytul; }
 Autor* Ksiazka::getAutor() { return autor; }
 std::string Ksiazka::getGatunek() { return gatunek; }
