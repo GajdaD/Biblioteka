99074d4e899eb93b98db5b46246828b17cce5023
mBisaga13 <mbisaga@student.agh.edu.pl>
2026-06-13 11:38:35 +0200

    Update Egzemplarz.cpp

--- src/Egzemplarz.cpp
+++ src/Egzemplarz.cpp
@@ -1,9 +1,11 @@
 #include "Egzemplarz.h"
-Egzemplarz::Egzemplarz(Ksiazka* ksiazka, std::string lokalizacja, int numer) : ksiazka(ksiazka), lokalizacja(lokalizacja), numer(numer) {}
+Egzemplarz::Egzemplarz(Ksiazka* ksiazka, std::string lokalizacja, int numer) : ksiazka(ksiazka), status(1), lokalizacja(lokalizacja), numer(numer) {}
 void Egzemplarz::zmienStatus(int s) {
     status = s;
 }
-void Egzemplarz::zarezerwujEgzemplarz(std::string od_date, std::string do_date) {}
+void Egzemplarz::zarezerwujEgzemplarz(std::string od_date, std::string do_date) {
+    status = 2;
+}
 Ksiazka* Egzemplarz::getKsiazka() { return ksiazka; }
 int Egzemplarz::getStatus() { return status; }
 std::string Egzemplarz::getLokalizacja() { return lokalizacja; }
